96a7a03ed6e0a7ec777939e26a9e8ffebdea191d
Teodora Rogozenco <teodora@rogozenco.de>
2022-02-27 11:17:02 +0100

    added new class MasterNode

---
+++ src/main/java/MasterNode.java
@@ -0,0 +1,53 @@
+import org.drasyl.node.DrasylConfig;
+import org.drasyl.node.DrasylConfigException;
+import org.drasyl.node.DrasylException;
+import org.drasyl.node.DrasylNode;
+import org.drasyl.node.event.Event;
+import org.drasyl.node.event.MessageEvent;
+import org.drasyl.node.event.NodeOnlineEvent;
+
+//import org.json.simple.JSONObject;
+
+public class MasterNode extends DrasylNode {
+
+    private boolean online = false;
+
+    public MasterNode() throws DrasylException {
+        super();
+    }
+
+    public MasterNode(DrasylConfig config) throws DrasylConfigException, DrasylException {
+        super(config);
+    }
+
+    @Override
+    public void onEvent(Event event) { //event-listener
+        if (event instanceof NodeOnlineEvent) {
+            online = true;
+        }
+        if (event instanceof MessageEvent) {
+
+            Object payload = ((MessageEvent) event).getPayload();
+            MessageRequest messagePayload = (MessageRequest) payload;
+            //switch (messagePayload.getRequest())
+
+        }
+    }
+    private void read() {
+
+    }
+
+    private void write() {
+
+    }
+
+    private void update() {
+
+    }
+
+    private void delete() {
+
+    }
+
+
+}
--- src/main/java/MessageRequest.java
+++ src/main/java/MessageRequest.java
@@ -1,18 +1,24 @@
 import java.lang.reflect.Array;
 
+
 public class MessageRequest
 {
-    private final String messageRequest;  //GET, POST, PACTH, REMOVE
+    private final String messageRequest;  //GET, POST, PATCH, REMOVE
     private final Array metadata;
     private final Message content;
 
 
     public MessageRequest(String messageRequest, Array metadata,
-            Message content)
+                          Message content)
     {
         this.messageRequest = messageRequest;
         this.metadata = metadata;
         this.content = content;
-        //dsdsds
+
     }
+
+    public String getRequest() {
+        return messageRequest;
+    }
+
 }
