284471f8f81b124301ca9b2db826af7a9b7c4ea5
Teodora Rogozenco <teodora@rogozenco.de>
2022-02-28 11:19:14 +0100

    added fromString() and toString() to class MessageRequest, changed type of metadata from array to RequestNumber

--- src/main/java/MessageRequest.java
+++ src/main/java/MessageRequest.java
@@ -6,17 +6,17 @@ import java.lang.reflect.Array;
 public class MessageRequest
 {
     private final Request messageRequest;  //GET, POST, PATCH, REMOVE
-    private final Array metadata;
+    private final RequestNumber metadata;
     private final Message content;
+    private static final String limiter = ";";
 
 
-    public MessageRequest(Request messageRequest, Array metadata,
+    public MessageRequest(Request messageRequest, RequestNumber metadata,
                           Message content)
     {
         this.messageRequest = messageRequest;
         this.metadata = metadata;
         this.content = content;
-
     }
 
     public Request getRequest() {
@@ -27,7 +27,26 @@ public class MessageRequest
         return content;
     }
 
-    public Array getMetadata() {
+    public RequestNumber getMetadata() {
         return metadata;
     }
+
+    public static MessageRequest fromString(String s){
+
+        String[] splits = s.split(limiter);
+        //kommt was sinnvolles raus?
+        //wenn nein, return null
+        //wenn ja:
+        Request createdMessageRequest = Request.GET; //nicht nur get, was auch immer split rausgibt
+        RequestNumber createdRequestNumber = new RequestNumber(36); //nicht 36, was auch immer split ausgibt
+        Message createdContent = new Message(1, "",""); //was auch immer split ausgibt
+        return new MessageRequest(createdMessageRequest, createdRequestNumber, createdContent);//
+    }
+
+    @Override
+    public String toString(){
+        //... (messageRequest to String, metadata + to String ..., content to String)
+        return limiter + limiter;
+    }
+
 }
