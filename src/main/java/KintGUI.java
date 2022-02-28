f7f47056f5c8975e2b60375899e1ee24c53558df
Nille0815 <nils_rogge@hotmail.de>
2022-02-28 12:43:21 +0100

    GUI

--- src/main/java/KintGUI.java
+++ src/main/java/KintGUI.java
@@ -1,12 +1,78 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class KintGUI
 {
-    private JButton turnOfButton;
-    private JButton addInHashMapButton;
+    private JButton Read;
+    private JTextField Key;
+    private JTextField Value;
+    private JTextField AntwortText;
+    private JButton Delete;
+    private JButton Write;
+    private JButton Update;
+    private JButton HeartBeat;
 
-    public KintGUI(){
+    public KintGUI(JButton heartBeat){
 
+        Delete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        Read.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        Write.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        Update.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        HeartBeat.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        Key.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        Value.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        AntwortText.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
 
     }
+
 }
