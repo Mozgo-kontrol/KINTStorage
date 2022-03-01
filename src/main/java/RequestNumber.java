25df8ca49c46a5181ed9cbc6c2c5f9cbc36f56f8
Igor Ferbert <ferniks92@gmail.de>
2022-03-01 13:00:46 +0100

    Send message Json Method 4

--- src/main/java/RequestNumber.java
+++ src/main/java/RequestNumber.java
@@ -1,12 +1,20 @@
+import lombok.Getter;
+import lombok.Setter;
+
 //all new
+@Setter
+@Getter
 public class RequestNumber {
 
-    private final int requestNumber;
+    private int requestNumber;
 
     RequestNumber(int i) {
         requestNumber = i;
     }
 
+    public RequestNumber()
+    {
+    }
     public int getRequestNumber() {
         return requestNumber;
     }
